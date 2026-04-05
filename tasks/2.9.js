7633dc3022827acae80597a28174943d948fb2f3
Alexey <lexanav1245@mail.ru>
2026-04-05 17:16:55 +0300

    split on functions

--- tasks/2.9.js
+++ tasks/2.9.js
@@ -3,35 +3,44 @@ function isTransformationPosible(initial, final) {
         return false;
     }
 
-    let initialMap = new Map();
-    let finalMap = new Map();
+    let initialMap = MakeMapWithElementsNumber(initial);
+    let finalMap = MakeMapWithElementsNumber(final);
 
-    for (let i = 0; i < initial.length; ++i) {
-        if (initialMap.has(initial[i])) {
-            initialMap.set(initial[i], initialMap.get(initial[i]) + 1);
-        } else {
-            initialMap.set(initial[i], 1);
-        }
+    return CompareMapsWithElementsNumber(initialMap, finalMap);
+}
 
-        if (finalMap.has(final[i])) {
-            finalMap.set(final[i], finalMap.get(final[i]) + 1);
+function MakeMapWithElementsNumber(list) {
+    let map = new Map();
+
+    for (let i = 0; i < list.length; ++i) {
+        if (map.has(list[i])) {
+            map.set(list[i], map.get(list[i]) + 1);
         } else {
-            finalMap.set(final[i], 1);
+            map.set(list[i], 1);
         }
     }
 
-    for (let entry of initialMap.entries()) {
-        if (!finalMap.has(entry[0])) {
+    return map;
+}
+
+function CompareMapsWithElementsNumber(first, second) {
+    if (first.size != second.size) {
+        return false;
+    }
+
+    for (let entry of first.entries()) {
+        if (!second.has(entry[0])) {
             return false;
         }
 
-        if (finalMap.get(entry[0]) != entry[1]) {
+        if (second.get(entry[0]) != entry[1]) {
             return false;
         }
     }
 
     return true;
 }
+
 
 const test_data = [
     {
