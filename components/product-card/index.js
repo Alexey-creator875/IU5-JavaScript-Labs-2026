bbafa8252e19a49f401c1767c972e8a767835913
Alexey <lexanav1245@mail.ru>
2026-04-28 11:50:53 +0300

    add arrow

--- components/product-card/index.js
+++ components/product-card/index.js
@@ -16,6 +16,12 @@ export class ProductCardComponent {
                         <span class="card-info">${data.description}</span>
                         <span class="card-info">Стоимость: ${data.price}</span>
                     </span>
+
+                    <span class="arr">
+                        <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22 7.5C22.7251 7.5 23.3301 8.01444 23.4697 8.69824C23.4897 8.79586 23.5 8.89746 23.5 9.00098V22.001C23.4997 22.8292 22.8283 23.501 22 23.501C21.1717 23.501 20.5003 22.8292 20.5 22.001V12.6221L10.0605 23.0615C9.47484 23.647 8.52518 23.6469 7.93945 23.0615C7.35367 22.4757 7.35367 21.5252 7.93945 20.9395L18.3789 10.5H9C8.17157 10.5 7.5 9.82843 7.5 9C7.5 8.17157 8.17157 7.5 9 7.5H22Z" fill="currentColor"></path>
+                        </svg>
+                    </span>
                 </div>
             `
         )
